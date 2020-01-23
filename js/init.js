6f3f475eaedc07c5de815f6556dc3cde483da7db
Marcos Zurck <marcoszurck@hotmail.com>
2020-01-22 22:06:51 -0300

    Banco de dados

---
+++ js/init.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAuAOR80uRHGEZj9IY2euFFX1ZFP5lEwH8",
+    authDomain: "linkme-655c0.firebaseapp.com",
+    databaseURL: "https://linkme-655c0.firebaseio.com",
+    projectId: "linkme-655c0",
+    storageBucket: "linkme-655c0.appspot.com",
+    messagingSenderId: "122207608206",
+    appId: "1:122207608206:web:2b27760b8e581e48c218df"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
